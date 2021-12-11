2e0db30dcb32ba39502f5c01464f5aa18a2ceca9
Christopher Lawrence Mallison Webb <cwebb15@gmail.com>
2021-12-11 17:23:31 +0200

    This is a final solution that provides the correct answer for the challenge.

--- Program.cs
+++ Program.cs
@@ -15,19 +15,30 @@ namespace D3T1
         static void Main(string[] args)
         {
             string filename = "input.txt";
-
-            //CheckFile(filename);
-
+                        
             int numRows = CountRows(filename);
             string[] arrayRows = new string[numRows];
             GetRows(filename, arrayRows);
-
             int numCols = CountCols(filename, arrayRows);
-
             char[,] dataArray = new char[numRows, numCols];
-
-
             GetArray(arrayRows, numRows, dataArray);
+
+            int gamma = 0;
+            gamma = GetGamma(dataArray, numCols, numRows);
+
+            int epsilon = 0;
+            epsilon = GetEpsilon(dataArray, numCols, numRows);
+
+            //WriteLine(dataArray[1,11]);
+            //WriteLine("Num Rows " + numRows);
+            //WriteLine("Num Cols " + numCols);
+            WriteLine(gamma);
+            WriteLine(epsilon);
+
+            int powerConsumption = gamma * epsilon;
+            WriteLine(powerConsumption);
+
+            ReadLine();
 
 
         }
@@ -57,8 +68,7 @@ namespace D3T1
                 {
                     dataArray [i, j] = tempRow [j];
                 }
-            }
-            
+            }     
             
         }
 
@@ -80,6 +90,79 @@ namespace D3T1
                 x++;
             }
             SR.Close();
+        }
+
+        private static int GetGamma(char[,] dataArray, int numCols, int numRows)
+        {
+            string binaryNumStr = "";
+            
+            for (int j = 0;j < numCols; j++)
+            {
+                int countOnes = 0;
+                int countZeros = 0;
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    if (dataArray[i,j] == '0')
+                    {
+                        countZeros++;
+                    }
+                    else if (dataArray[i, j] == '1')
+                    {
+                        countOnes++;
+                    }
+                    else
+                    {
+                        WriteLine("Error");                    }
+                }
+
+                if (countZeros > countOnes)
+                    binaryNumStr = binaryNumStr + "0";
+                else binaryNumStr = binaryNumStr + "1";
+
+                
+            }
+
+            int gamma = Convert.ToInt32(binaryNumStr, 2);
+            return gamma;
+
+        }
+
+        private static int GetEpsilon(char[,] dataArray, int numCols, int numRows)
+        {
+            string binaryNumStr = "";
+
+            for (int j = 0; j < numCols; j++)
+            {
+                int countOnes = 0;
+                int countZeros = 0;
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    if (dataArray[i, j] == '0')
+                    {
+                        countZeros++;
+                    }
+                    else if (dataArray[i, j] == '1')
+                    {
+                        countOnes++;
+                    }
+                    else
+                    {
+                        WriteLine("Error");
+                    }
+                }
+
+                if (countZeros > countOnes)
+                    binaryNumStr = binaryNumStr + "1";
+                else binaryNumStr = binaryNumStr + "0";
+
+
+            }
+
+            int gamma = Convert.ToInt32(binaryNumStr, 2);
+            return gamma;
+
         }
     }
 }
